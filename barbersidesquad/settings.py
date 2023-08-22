abbbcc4fa92d08e317461f759f62b6e96c96111f
alexflip1990 <>
2023-08-22 14:50:46 +0000

    amended allowed_hosts

--- barbersidesquad/settings.py
+++ barbersidesquad/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['barberside-squad.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['barberside-squad-e010ddf0fb8f.herokuapp.com', 'localhost']
 
 
 # Application definition
